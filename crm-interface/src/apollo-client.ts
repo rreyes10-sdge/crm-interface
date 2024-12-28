ba43a25640fce6fcc7e2195f1a64b805e4e4039a
Rafael <rreyes10@sdgecontractor.com>
2024-12-27 16:22:07 -0800

    fixed apollo client url

--- crm-interface/src/apollo-client.ts
+++ crm-interface/src/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http/://127.0.0.1:5000/graphql',
+  uri: 'http://127.0.0.1:5000/graphql',
   cache: new InMemoryCache(),
 });
 
