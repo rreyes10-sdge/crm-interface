d27709e0687a416b936b6398266205ff9c654bcf
Rafael <rreyes10@sdgecontractor.com>
2025-01-21 14:10:18 -0800

    updated logged activities query

--- crm-interface/graphql_app/queries.py
+++ crm-interface/graphql_app/queries.py
@@ -209,7 +209,7 @@ SELECT
     ds.Date AS LogDate,
     COUNT(a.ActivityId) AS ActivityCount
 FROM DateSeries ds
-LEFT JOIN cleantranscrm.Activity a ON DATE(a.CreatedAt) = ds.Date AND (a.ActivityTypeId IN (1, 2, 3, 4, 5, 6) OR a.ActivityId IS NULL) AND a.ProjectId in (select projectid from cleantranscrm.Project p where ProgramId = 16) 
+LEFT JOIN cleantranscrm.Activity a ON DATE(a.CreatedAt) = ds.Date AND (a.ActivityTypeId IN (1, 2, 3, 4, 5, 6) OR a.ActivityTypeId IS NULL) AND a.ProjectId in (select projectid from cleantranscrm.Project p where ProgramId = 16) 
 LEFT JOIN cleantranscrm.Project p ON p.ProjectId = a.ProjectId AND p.ProgramId = 16
 WHERE ds.Date >= CURDATE() - INTERVAL 30 DAY
 GROUP BY ds.Date
@@ -229,10 +229,9 @@ ActivityCounts AS (
         ds.Date AS LogDate,
         COALESCE(COUNT(a.ActivityId), 0) AS ActivityCount
     FROM DateSeries ds
-    LEFT JOIN cleantranscrm.Activity a ON DATE(a.CreatedAt) = ds.Date AND a.ProjectId in (select projectid from cleantranscrm.Project p where ProgramId = 16) 
+    LEFT JOIN cleantranscrm.Activity a ON DATE(a.CreatedAt) = ds.Date AND (a.ActivityTypeId IN (1, 2, 3, 4, 5, 6) OR a.ActivityTypeId IS NULL) AND a.ProjectId in (select projectid from cleantranscrm.Project p where ProgramId = 16) 
     LEFT JOIN cleantranscrm.Project p ON p.ProjectId = a.ProjectId
-    WHERE a.ActivityTypeId IN (1, 2, 3, 4, 5, 6)
-      AND ds.Date >= CURDATE() - INTERVAL 30 DAY
+    WHERE ds.Date >= CURDATE() - INTERVAL 30 DAY
     GROUP BY ds.Date
     ORDER BY ds.Date
 ),
